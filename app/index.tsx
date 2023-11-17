943d015547e6b0d44069648627f3d612536a14c4
Higor Grigorio <higorgrigorio@gmail.com>
2023-11-17 10:03:51 -0300

    first commit

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,10 @@
-import { YStack, Text } from "tamagui";
+import {Button, YStack} from "tamagui";
+import AppLayout from './_layout'
 
 export default function App() {
-  return (
-    <YStack jc="center" ai="center">
-      <Text color="$red10" fontSize="$10" fontFamily="$body">
-        Hello tamagui with expo-router!
-      </Text>
-    </YStack>
-  );
+    return (
+        <YStack jc="center" ai="center">
+            <Button size="$5">Click me</Button>
+        </YStack>
+    );
 }
